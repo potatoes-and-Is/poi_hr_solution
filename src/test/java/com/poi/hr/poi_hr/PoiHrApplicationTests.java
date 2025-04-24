421374bf55f33b320bcb7752b303e034626d5825
minyeong-lee <lmy1366@gmail.com>
2025-04-24 17:22:22 +0900

    first commit

---
+++ src/test/java/com/poi/hr/poi_hr/PoiHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poi.hr.poi_hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
